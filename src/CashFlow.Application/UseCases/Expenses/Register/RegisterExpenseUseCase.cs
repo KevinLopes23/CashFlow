423930f22b8141b58aafe0fca783c2569dcbcec2
Kevin Lopes <kevinlopes2304@gmail.com>
2024-12-11 12:40:34 -0300

    Validação Despesas

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.requests;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
@@ -6,8 +7,37 @@ public class RegisterExpenseUseCase
 {
     public ResquestRegisterExpenseJson Execute(ResquestRegisterExpenseJson resquest)
     {
-        //TO DO VALIDACIOS
+        Validate(resquest);
 
         return new ResquestRegisterExpenseJson();
+    }
+
+    private void Validate(ResquestRegisterExpenseJson resquest)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(resquest.Title);
+
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required");
+        }
+
+        if (resquest.Amount <= 0)
+        {
+            throw new ArgumentException("The Amount must be greater than zero ");
+        }
+
+
+        var result = DateTime.Compare(resquest.Date, DateTime.UtcNow);
+        if (result > 0)
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), resquest.PaymentType);
+
+        if (paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("Payment type is not valid");
+        }
     }
 }
