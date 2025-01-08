3ddb0258395238a494aced64436cc1bba90820af
Kevin Lopes <kevinlopes2304@gmail.com>
2025-01-08 08:29:50 -0300

    Criando um teste para um erro

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.requests;
+using CashFlow.Exception;
 using CommonTestUtilites.Requets;
 using FluentAssertions;
 
@@ -21,6 +22,23 @@ namespace Validators.Tests.Expenses.Register
             //Assert
             result.IsValid.Should().BeTrue();
 
+        }
+        [Fact]
+        public void Error_Title_Empty()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = ResquestRegisterExpenseJsonBuilder.Build();
+            request.Title = string.Empty;
+            request.Amount = -1;
+
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.TITLE_REQUIRED));
         }
 
     }
